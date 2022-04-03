8b2fe3dfec55ffce9b486c0b026fddb44e9d1096
Taeju Kwon <taeju.kwon@digipen.edu>
2022-04-03 12:45:30 +0900

    39.9점

--- Blist/Blist/BList.cpp
+++ Blist/Blist/BList.cpp
@@ -1,4 +1,4 @@
-#include "BList.h"
+ï»¿#include "BList.h"
 
 // default constructor
 template<typename T, int Size>
@@ -22,7 +22,7 @@ BList<T, Size>::BList(const BList& rhs) : stats_(rhs.stats_)
 	{
 		curr = new BNode;
 
-		for (unsigned int j = 0; j < stats_.ArraySize; j++)
+		for (int j = 0; j < stats_.ArraySize; j++)
 		{
 			curr->values[j] = hnode->values[j];
 			curr->count = hnode->count;
@@ -65,7 +65,7 @@ BList<T, Size>& BList<T, Size>::operator=(const BList& rhs)
 	{
 		curr = new BNode;
 
-		for (unsigned int j = 0; j < stats_.ArraySize; j++)
+		for (int j = 0; j < stats_.ArraySize; j++)
 		{
 			curr->values[j] = hnode->values[j];
 			curr->count = hnode->count;
@@ -136,17 +136,17 @@ void BList<T, Size>::push_front(const T& value)
 	else {
 		int useitem = head_->count;
 
-		if (useitem < stats_.ArraySize) // ÇöÀç »ç¿ëÁßÀÎ ³ëµå
+		if (useitem < stats_.ArraySize) // Ã‡Ã¶Ã€Ã§ Â»Ã§Â¿Ã«ÃÃŸÃ€Ã Â³Ã«ÂµÃ¥
 		{
 			for (int i = useitem; i > 0; i--)
 			{
-				head_->values[i] = head_->values[i-1];
+				head_->values[i] = head_->values[i - 1];
 			}
 			head_->values[0] = value;
 			head_->count++;
 			stats_.ItemCount++;
 		}
-		else //»ç¿ëÁß ³ëµå ¾øÀ½
+		else //Â»Ã§Â¿Ã«ÃÃŸ Â³Ã«ÂµÃ¥ Â¾Ã¸Ã€Â½
 		{
 			BNode* curr = new BNode;
 
@@ -162,17 +162,17 @@ void BList<T, Size>::push_front(const T& value)
 }
 
 template<typename T, int Size>
-void BList<T, Size>::split_node(BNode* curr, T value)
+void BList<T, Size>::split_node(BNode* curr)
 {
-	//pushback ¾ÈÇÑ¹öÀü
+	//pushback Â¾ÃˆÃ‡Ã‘Â¹Ã¶Ã€Ã¼
 	BNode* newnode = new BNode;
 	newnode->prev = curr;
-	
+
 	if (Size != 1)
 	{
 		int setsize = Size / 2;
-		//³ëµå¸¦ ¸¸µé¾î¼­ ¾ÈÀÇ µ¥ÀÌÅÍ¸¦ ³ª´²ÁÜ
-		//µ¥ÀÌÅÍ¸¦ ¿Ã¹Ù¸¥ À§Ä¡¿¡ ³ÖÀº°Ç ¾Æ´Ô -> ±×³É split¸¸ ÇØÁÜ
+		//Â³Ã«ÂµÃ¥Â¸Â¦ Â¸Â¸ÂµÃ©Â¾Ã®Â¼Â­ Â¾ÃˆÃ€Ã‡ ÂµÂ¥Ã€ÃŒÃ…ÃÂ¸Â¦ Â³ÂªÂ´Â²ÃÃœ
+		//ÂµÂ¥Ã€ÃŒÃ…ÃÂ¸Â¦ Â¿ÃƒÂ¹Ã™Â¸Â¥ Ã€Â§Ã„Â¡Â¿Â¡ Â³Ã–Ã€ÂºÂ°Ã‡ Â¾Ã†Â´Ã” -> Â±Ã—Â³Ã‰ splitÂ¸Â¸ Ã‡Ã˜ÃÃœ
 		int value_ = 0;
 		for (int i = setsize; i < Size; i++)
 		{
@@ -199,13 +199,13 @@ void BList<T, Size>::insert(const T& value)
 	push_back(value);
 
 	BNode* curr = head_;
-	int first = 0;
+	// unsigned int first = 0;
 
 	if (curr != nullptr)
 	{
 		if (Size == 1)
 		{
-			//sort Ãß°¡
+			//sort ÃƒÃŸÂ°Â¡
 			if (stats_.ItemCount == 2)
 			{
 				if (value < curr->values[0])
@@ -270,8 +270,8 @@ void BList<T, Size>::insert(const T& value)
 
 			if (checkfullnode(curr) == true)
 			{
-				split_node(curr, value);
-				//sortÃß°¡
+				split_node(curr);
+				//sortÃƒÃŸÂ°Â¡
 				for (int i = 0; i < stats_.NodeCount; i++)
 				{
 					for (int j = 0; j < curr->count; j++)
@@ -281,7 +281,7 @@ void BList<T, Size>::insert(const T& value)
 							for (int k = curr->count - j; k > 1; k--)
 							{
 								curr->values[k + 1] = curr->values[k];
-								curr->values[k];
+								curr->values[k] = value;
 								curr->count++;
 								stats_.ItemCount++;
 							}
@@ -300,7 +300,7 @@ void BList<T, Size>::insert(const T& value)
 							for (int k = curr->count - j; k > 1; k--)
 							{
 								curr->values[k + 1] = curr->values[k];
-								curr->values[k];
+								curr->values[k] = value;
 								curr->count++;
 								stats_.ItemCount++;
 							}
@@ -316,20 +316,140 @@ void BList<T, Size>::insert(const T& value)
 template<typename T, int Size>
 void BList<T, Size>::remove(int index)
 {
+	BNode* curr = head_;
+	int sum = 0;
+
+	while (sum <= index)
+	{
+		sum += curr->count;
+		curr = curr->next;
+	}
+
+	if (curr == head_)
+	{
+		curr = curr;
+	}
+	else if (curr)
+	{
+		curr = curr->prev;
+	}
+	else {
+		curr = tail_;
+	}
+
+	///nsigned int m = index % Size;
+
+	for (unsigned int i = 0; i < Size; i++)
+	{
+		for (int j = i; j < curr->count; j++)
+		{
+			curr->values[j] = curr->values[j + 1];
+		}
+	}
+	--curr->count;
+	--stats_.ItemCount;
+
+	if (curr->count == 0) //delete node
+	{
+		for (int j = 0; j < Size; ++j)
+		{
+			if (curr->prev)
+			{
+				curr->prev->next = curr->next;
+			}
+			else {
+				head_ = curr->next;
+			}
+
+			if (curr->next)
+			{
+				curr->next->prev = curr->prev;
+			}
+			else {
+				tail_ = curr->prev;
+			}
+
+			curr->~BNode();
+			free(curr);
+		}
+	}
 
 }
 
 template<typename T, int Size>
 void BList<T, Size>::remove_by_value(const T& value)
 {
+	auto deletion_index = 0;
+	auto curr = head_;
+	auto is_broken = false;
 
+	while (curr) {
+		for (auto i = 0; i < curr->count; ++i)
+			if (curr->values[i] == value) {
+				deletion_index = i;
+				is_broken = true;
+				break;
+			}
+		if (is_broken)
+			break;
+
+		curr = curr->next;
+	}
+
+	if (curr != nullptr) {
+		// delete element
+		for (auto j = deletion_index; j < curr->count; ++j)
+			curr->values[j] = curr->values[j + 1];
+		--curr->count;
+		--stats_.ItemCount;
+
+		if (curr->count == 0) {
+			for (int j = 0; j < Size; ++j)
+			{
+				if (curr->prev)
+				{
+					curr->prev->next = curr->next;
+				}
+				else {
+					head_ = curr->next;
+				}
+
+				if (curr->next)
+				{
+					curr->next->prev = curr->prev;
+				}
+				else {
+					tail_ = curr->prev;
+				}
+
+				curr->~BNode();
+				free(curr);
+			}
+			--stats_.NodeCount;
+		}
+	}
 }
 
 // returns index, -1 if not found
 template<typename T, int Size>
 int BList<T, Size>::find(const T& value) const
 {
-	return 0;
+	BNode* curr = head_;
+	int sum = 0;
+
+	while (curr)
+	{
+		for (int i = 0; i < curr->count; i++)
+		{
+			if (curr->values[i] == value)
+			{
+				return sum + i;
+			}
+		}
+		sum += curr->count;
+		curr = curr->next;
+	}
+	return -1;
 }
 
 // for l-values
@@ -337,7 +457,7 @@ template<typename T, int Size>
 T& BList<T, Size>::operator[](int index)
 {
 	BNode* curr = head_;
-	int sum = stats_.ItemCount;
+	//unsigned int sum = stats_.ItemCount;
 	int j = 0;
 	int i = 0;
 
@@ -365,7 +485,7 @@ const T& BList<T, Size>::operator[](int index) const
 	for (i = index; i > 0; i--)
 	{
 		j++;
-		if (j == stats_.ItemCount)
+		if (j == sum)
 		{
 			curr = curr->next;
 		}
@@ -385,7 +505,7 @@ size_t BList<T, Size>::size() const
 template<typename T, int Size>
 void BList<T, Size>::clear()
 {
-	
+
 }
 
 // so the allocator knows the size
